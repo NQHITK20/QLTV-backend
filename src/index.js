daf2fcc3077c326c4492a690dcd00ca3a20f4701
Johny Ping <nqhitk20@gmail.com>
2024-05-23 20:15:05 +0700

    :V

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ import bodyParser from "body-parser";
 import initWebRoute from "./route/web";
 import connectDB from "./config/connectDB";
 import cors from 'cors';
+import Sequelize from 'sequelize';
+import mysql2 from 'mysql2';
+
 
 require('dotenv').config();
+
+ // Needed to fix sequelize issues with WebPack
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'mysql',
+    dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+    host: process.env.DB_HOST,
+    port: process.env.PORT
+  }
+)
+
+export async function connectToDatabase() {
+  console.log('Trying to connect via sequelize')
+  await sequelize.sync()
+  await sequelize.authenticate()
+  console.log('=> Created a new connection.')
+
+  // Do something 
+}
+
 
 let app = express();
 
