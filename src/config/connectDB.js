1432303734897a8e98170a4af2f90a2e13bfab06
Johny Ping <nqhitk20@gmail.com>
2024-05-30 16:45:29 +0700

    :V

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql2',
+  dialect: 'mysql',
   dialectModule: require('mysql2'),
   pool: {
     max: parseInt(process.env.DB_POOL_MAX) || 15,
