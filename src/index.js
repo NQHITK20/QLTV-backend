7aac99bf753a20d4a1807a3234f55af96cf20005
Johny Ping <nqhitk20@gmail.com>
2024-05-30 15:51:00 +0700

    :V

--- src/index.js
+++ src/index.js
@@ -16,27 +16,32 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Kết nối tới cơ sở dữ liệu và sau đó khởi tạo các route của ứng dụng
-connectToDatabase().then(() => {
-  // Sử dụng middleware CORS sau khi kết nối tới cơ sở dữ liệu thành công
-  const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  };
-  app.use(cors(corsOptions));
+(async () => {
+  try {
+    await connectToDatabase();
 
-  // Khởi tạo các route của ứng dụng sau khi kết nối tới cơ sở dữ liệu thành công
-  initWebRoute(app);
+    // Sử dụng middleware CORS sau khi kết nối tới cơ sở dữ liệu thành công
+    const corsOptions = {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204
+    };
+    app.use(cors(corsOptions));
 
-  // Lắng nghe các kết nối tới cổng PORT hoặc cổng mặc định 8000
-  const port = process.env.PORT || 3306;
-  app.listen(port, () => {
-    console.log(`Backend nodejs is running on port ${port}`);
-  });
-}).catch(error => {
-  console.error('Failed to start the server:', error);
-});
+    // Khởi tạo các route của ứng dụng sau khi kết nối tới cơ sở dữ liệu thành công
+    initWebRoute(app);
+
+    // Lắng nghe các kết nối tới cổng PORT hoặc cổng mặc định 8000
+    const port = process.env.PORT || 3306;
+    app.listen(port, () => {
+      console.log(`Backend nodejs is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to start the server:', error);
+  }
+})();
+
 
 // Xuất khẩu ứng dụng Express để sử dụng trong các test hoặc mục đích khác
 module.exports = app;
