ae0c8d3b55019e7f89d496fe21cb83e31259c7c7
Johny Ping <nqhitk20@gmail.com>
2024-05-30 10:34:59 +0700

    :V

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,6 +2,8 @@
 
 const Sequelize = require('sequelize');
 const mysql2 = require('mysql2');
+const retry = require('async-retry');
+
 require('dotenv').config();
 
 //Test
@@ -39,7 +41,8 @@ const sequelize = new Sequelize(
       idle: parseInt(process.env.DB_POOL_IDLE) || 10000
     },
     dialectOptions: {
-    connectTimeout: 60000 // 60 seconds
+    connectTimeout: 60000, // 60 seconds
+    keepAlive: true
   }
   }
 );
@@ -57,5 +60,37 @@ async function connectToDatabase() {
     throw error;
   }
 }
+
+async function connectWithRetry() {
+  await retry(async () => {
+    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      dialectOptions: {
+        connectTimeout: 60000, // 60 giây
+        keepAlive: true
+      },
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 60000, // 60 giây
+        idle: 10000     // 10 giây
+      }
+    });
+
+    try {
+      await sequelize.authenticate();
+      console.log('Kết nối đã được thiết lập thành công.');
+    } catch (error) {
+      console.error('Không thể kết nối tới cơ sở dữ liệu:', error);
+      throw error;
+    }
+  }, {
+    retries: 5,     // Số lần thử lại
+    minTimeout: 1000 // Thời gian chờ tối thiểu giữa các lần thử lại
+  });
+}
+
+connectWithRetry();
 
 module.exports = connectToDatabase;
