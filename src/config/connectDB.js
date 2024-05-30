b4b9a1d711e0e146f8525373b5e20cd8a8e1ee9a
Johny Ping <nqhitk20@gmail.com>
2024-05-30 15:43:51 +0700

    :V

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   logging: true,
+  pool: {
+    max: parseInt(process.env.DB_POOL_MAX) || 15,
+    min: parseInt(process.env.DB_POOL_MIN) || 5,
+    acquire: parseInt(process.env.DB_POOL_ACQUIRE) || 20000,
+    idle: parseInt(process.env.DB_POOL_IDLE) || 30000
+  }
 });
 let connectToDatabase = async () => {
   try {
