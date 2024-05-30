20208dbefed0d1b46c6d276991cf4ae0f0d7c2df
Johny Ping <nqhitk20@gmail.com>
2024-05-30 09:49:34 +0700

    :V

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -37,7 +37,10 @@ const sequelize = new Sequelize(
       min: parseInt(process.env.DB_POOL_MIN) || 0,
       acquire: parseInt(process.env.DB_POOL_ACQUIRE) || 30000,
       idle: parseInt(process.env.DB_POOL_IDLE) || 10000
-    }
+    },
+    dialectOptions: {
+    connectTimeout: 60000 // 60 seconds
+  }
   }
 );
 
