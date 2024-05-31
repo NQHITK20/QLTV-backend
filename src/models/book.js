c21b61f1e62af2c4fc30cdf62e046b77c113aca2
Johny Ping <nqhitk20@gmail.com>
2024-05-31 15:56:21 +0700

    :V

--- src/models/book.js
+++ src/models/book.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            Book.belongsTo(models.Category, { foreignKey: 'categoryId' });
+            Book.belongsTo(models.Category, { foreignKey: 'category' });
         }
     };
     Book.init({
