57ba9bbcf442d6c73ccc2fd25adb6b4bd6550493
Johny Ping <nqhitk20@gmail.com>
2024-06-24 17:04:00 +0700

    :V

---
+++ src/migrations/mgr-news.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { first, last } = require("lodash");
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('news', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            image: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            author: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            publicAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            },
+            createdAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('news');
+    }
+};
