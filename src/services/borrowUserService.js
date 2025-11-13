4fc8321e440347fb4ce42e0a5328059e6c952ffc
norman <nqhitk20>
2025-11-13 19:38:19 +0700

    :V

--- src/services/borrowUserService.js
+++ src/services/borrowUserService.js
@@ -9,11 +9,12 @@ let createBorrowUser = (data) =>{
                     borrowcode:data.orderCode,
                     dateborrow:data.timeborrow,
                     datareturn:data.timereturn,
-                    borrowstatus:'Đang mượn',
+                    borrowstatus:'Đang xử lý',
                     description: data.description,
                 }),
                 resolve({
                     errCode: 0,
+                    errMessage: 'Tạo phiếu mượn thành công',
                 });
             
         } catch (e) {
